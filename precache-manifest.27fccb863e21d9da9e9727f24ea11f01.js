055484fbb520f03cb84408fc8c49e4e90651850f
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2019-10-29 16:59:57 -0300

    Updates

---
+++ precache-manifest.27fccb863e21d9da9e9727f24ea11f01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e8d9dd5c53db0e9764d24cc5ced0833",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "56f150050c7a692f2b9b",
+    "url": "/myapp/static/css/main.b0f69371.chunk.css"
+  },
+  {
+    "revision": "f2573bd5b1ec456c90be",
+    "url": "/myapp/static/js/2.86a589cc.chunk.js"
+  },
+  {
+    "revision": "56f150050c7a692f2b9b",
+    "url": "/myapp/static/js/main.cc52ff5b.chunk.js"
+  },
+  {
+    "revision": "8a5f5ab2899fb394bdeb",
+    "url": "/myapp/static/js/runtime-main.7134332c.js"
+  }
+]);
